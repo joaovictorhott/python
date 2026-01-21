f31d0069e56d1bc240e02c36ba044bdc175ff8ff
João Victor Hott <124474503+joaovictorhott@users.noreply.github.com>
2026-01-21 18:42:20 -0300

    Create zipper function to merge two lists
    Implement a function to merge two lists element by element, forming pairs.

---
+++ Merging_Two_Lists_in_Order.py
@@ -0,0 +1,19 @@
+#This exercise consists of creating a function called zipper, inspired by the concept of a clothing zipper.
+#The purpose of the function is to merge two lists element by element, in order, forming pairs of values.
+#The function must use all elements from the shorter list, ignoring any extra elements in the longer one
+#input > ['Salvador', 'Ubatuba', 'Belo Horizonte']     ['BA', 'SP', 'MG', 'RJ']
+#output > [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+
+def zipper(list1, list2):
+    result = []
+    min_length = min(len(list1), len(list2))
+
+    for i in range(min_length):
+        result.append((list1[i], list2[i]))
+
+    return result
+cities = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+states = ['BA', 'SP', 'MG', 'RJ']
+
+print(zipper(cities, states))
